d5a1e1d26b69a7aa12910b5049c7758550368b20
m-vdb <mvergerdelbove@work4labs.com>
2015-09-12 16:33:41 -0700

    display api version

--- ethic/routes.js
+++ ethic/routes.js
@@ -1,7 +1,11 @@
+var VERSION = '0.0.1';
+
 module.exports = {
+  version: VERSION,
   home: function (req, res, next) {
     res.send({
-      "name": "ethic"
+      "name": "ethic",
+      "version": VERSION
     });
     return next();
   }
