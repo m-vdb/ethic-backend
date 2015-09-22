6b3e46896ffa9ec4cd1b1d028dec37d9b06d7a67
m-vdb <mvergerdelbove@work4labs.com>
2015-09-22 19:25:15 +0200

    skeleton for Contract.getStorage + Contract.getMemberStorage

--- ethic/models/contract.js
+++ ethic/models/contract.js
@@ -16,7 +16,28 @@ contractSchema.static({
       else if (!contract) cb(new Error('Cannot find main contract.'));
       else cb(null, web3.eth.contract(contract.abi).at(contract.address));
     });
-  }
+  },
+  getStorage: function (key, cb) {
+    this.getMain(function (err, contract) {
+      if (err) return cb(err);
+
+      // TODO: have this in database
+      var storagePosition = contract.storageIndexes[key];
+      web3.eth.getStorageAt(contract.address, storagePosition, function (err, data) {
+        if (err) return cb(err);
+
+        // TODO: decode data
+        cb(null, data);
+      });
+    });
+  },
+  getMemberStorage: function (address, cb) {
+    this.getStorage('members', function (err, members) {
+      if (err) return cb(err);
+
+      cb(null, members[address]);
+    });
+  },
 });
 
 module.exports = mongoose.model('Contract', contractSchema);
