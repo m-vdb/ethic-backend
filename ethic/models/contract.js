900d91e10753f17b618ab5a3c232bcbc7a24bcea
m-vdb <mvergerdelbove@work4labs.com>
2015-10-03 08:41:08 -0700

    getMain attaches ABI to contract

--- ethic/models/contract.js
+++ ethic/models/contract.js
@@ -1,5 +1,8 @@
-var mongoose = require('mongoose'),
+var _ = require('underscore'),
+    mongoose = require('mongoose'),
     web3 = require('web3');
+
+var ethUtils = require('../utils/eth.js');
 
 var contractSchema = new mongoose.Schema({
   address: String,
@@ -14,18 +17,34 @@ contractSchema.static({
     return this.findOne({name: 'ethic_main'}, function (err, contract) {
       if (err) cb(err);
       else if (!contract) cb(new Error('Cannot find main contract.'));
-      else cb(null, contract.ethereumContract());
+      else cb(null, contract.attachAbi());
     });
   }
 });
 
-// TODO:
-//  - find a way to bind each method from the eth contract to mongo contract
-//  - have a configuration file (enough for now) with gas costs
-//  - implement transaction receipt check (while ...)
+// TODO: add gas costs in database
 contractSchema.method({
   ethereumContract: function () {
     return web3.eth.contract(this.abi).at(this.address);
+  },
+
+  attachAbi: function () {
+    _.each(this.abi, function (abi) {
+      var method, methodName;
+      if (abi.constant) {  // accessor
+        method = ethUtils.makeAccessor(abi);
+        methodName = 'get_' + abi.name;
+        // remove 's' for plurals, 'members' becomes 'getMember'
+        if (methodName[methodName.length - 1] == 's') methodName = methodName.substring(0, methodName.length - 2);
+      }
+      else {  // contract method
+        method = ethUtils.makeMethod(abi);
+        methodName = abi.name;
+      }
+      this[methodName] = method;  // prototype?
+      _.bind(method, this);
+    }, this)
+    return this;
   }
 });
 
