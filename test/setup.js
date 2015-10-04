24ed9c777523040f7f24a40142339e72e341e3a1
m-vdb <mvergerdelbove@work4labs.com>
2015-10-03 23:05:42 -0700

    use chai-fuzzy

--- test/setup.js
+++ test/setup.js
@@ -1,6 +1,8 @@
 var chai = require("chai");
 var sinonChai = require("sinon-chai");
+var chaiFuzzy = require("chai-fuzzy");
 chai.use(sinonChai);
+chai.use(chaiFuzzy);
 
 var web3Admin = require('../ethic/utils/web3-admin.js');
 web3Admin();
