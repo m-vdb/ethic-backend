17a3f2444a35eca53fc4d0974557ffaea2f3d757
m-vdb <mvergerdelbove@work4labs.com>
2015-10-08 12:52:38 -0400

    contract doesnt need databse, uses json file

--- ethic/models/contract.js
+++ ethic/models/contract.js
@@ -1,51 +1,33 @@
 var _ = require('underscore'),
-    mongoose = require('mongoose'),
     web3 = require('web3');
 
-var ethUtils = require('../utils/eth.js');
+var contractData = require('../data/contract.json'),
+    ethUtils = require('../utils/eth.js');
 
-var contractSchema = new mongoose.Schema({
-  address: String,
-  name: {type: String, index: {unique: true}},
-  abi: Array
-}, {
-  collection: 'contracts'
-});
+function Contract (data) {
+  _.extend(this, data);
+  this._attachAbi();
+}
 
-contractSchema.static({
-  getMain: function (cb) {
-    return this.findOne({name: 'ethic_main'}, function (err, contract) {
-      if (err) cb(err);
-      else if (!contract) cb(new Error('Cannot find main contract.'));
-      else cb(null, contract.attachAbi());
-    });
-  }
-});
-
-// TODO: add gas costs in database
-contractSchema.method({
+// TODO: add gas costs?
+_.extend(Contract.prototype, {
   ethereumContract: function () {
     return web3.eth.contract(this.abi).at(this.address);
   },
 
-  attachAbi: function () {
+  _attachAbi: function () {
+    var contract = this.ethereumContract();
     _.each(this.abi, function (abi) {
-      var method, methodName;
-      if (abi.constant) {  // accessor
-        method = ethUtils.makeAccessor(abi);
-        methodName = 'get_' + abi.name;
-        // remove 's' for plurals, 'members' becomes 'getMember'
-        if (methodName[methodName.length - 1] == 's') methodName = methodName.substring(0, methodName.length - 2);
-      }
-      else {  // contract method
-        method = ethUtils.makeMethod(abi);
-        methodName = abi.name;
-      }
-      this[methodName] = method;  // prototype?
-      _.bind(method, this);
+      var factory = abi.constant ? ethUtils.makeAccessor : ethUtils.makeMethod,
+          method = factory(contract, abi),
+          methodName = abi.name;
+
+      Contract.prototype[methodName] = method;
     }, this)
     return this;
   }
 });
 
-module.exports = mongoose.model('Contract', contractSchema);
+var main = new Contract(contractData);
+
+module.exports = main;
