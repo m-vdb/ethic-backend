9a3ea51aaa0bd542eaba823c402f830337cb86a9
m-vdb <mvergerdelbove@work4labs.com>
2015-10-18 09:59:43 -0700

    use ES6 class for BaseTask

--- ethic/tasks/base.js
+++ ethic/tasks/base.js
@@ -1,35 +1,33 @@
+"use strict";
 var queues = require('../queues.js');
 
 
-function BaseTask () {
-  this.taskName = this.constructor.name;
-}
-
-// class methods
-
-BaseTask.delay = function (data, queue, cb) {
-  if (typeof queue == 'function') {
-    cb = queue;
-    queue = null;
+class BaseTask {
+  constructor() {
+    this.taskName = this.constructor.name;
   }
 
-  queue = queue || 'main';
-  var job = queues[queue]
-    .create(this.name, data)
-    .save(cb);
+  static delay (data, queue, cb) {
+    if (typeof queue == 'function') {
+      cb = queue;
+      queue = null;
+    }
+
+    queue = queue || 'main';
+    var job = queues[queue]
+      .create(this.name, data)
+      .save(cb);
+  }
+
+  run (data, cb) {
+    console.log('Processing task:', this.taskName);
+    return this.process(data, cb);
+  }
+
+  process (data, cb) {  // to override in sub classes
+    cb();
+  }
 }
 
-// instance methods
-
-BaseTask.prototype.run = function (data, cb) {
-  console.log('Processing task:', this.taskName);
-  return this.process(data, cb);
-};
-
-
-// to override in sub classes
-BaseTask.prototype.process = function (data, cb) {
-  cb();
-};
 
 module.exports = BaseTask
