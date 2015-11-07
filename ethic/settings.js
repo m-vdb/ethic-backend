c898d3652b4053fcf3917eb2629ac6ef55ab3295
m-vdb <mvergerdelbove@work4labs.com>
2015-11-07 08:38:14 -0800

    add settings.cookieName

--- ethic/settings.js
+++ ethic/settings.js
@@ -4,6 +4,7 @@ var _ = require('underscore');
 module.exports = {
   version: '0.0.1',
   authSecret: 'i-am-a-secret-yeah',
+  cookieName: 'ethic',  // TODO: this should depend on the env
   mongoUri: 'mongodb://localhost/ethic',
   mongoOptions: {},
   contractTypes: _.keys(require('./contracts')),
