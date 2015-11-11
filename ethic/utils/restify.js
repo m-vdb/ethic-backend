fcf76c77c420a4e4e3cf2589d9e0caeb4ddd1cd7
m-vdb <mvergerdelbove@work4labs.com>
2015-11-11 09:18:41 -0800

    improve restify utils + more tests

--- ethic/utils/restify.js
+++ ethic/utils/restify.js
@@ -1,14 +1,27 @@
 var restify = require('restify');
 
+var RestifyValidator = require('./restify-validator.js');
+
 module.exports = function () {
   return function(req, res, next) {
+
+    req.hasValidationErrors = function () {
+      return req.validationErrors && req.validationErrors.length > 0;
+    };
+
     req.sendValidationErrorIfAny = function () {
-      if (req.validationErrors()) {
+      if (req.hasValidationErrors()) {
         res.send(new restify.errors.BadRequestError("Bad parameters."));
         return true;
       }
       return false;
-    }
+    };
+
+    req.assert = function (param, msg) {
+      req.validationErrors = req.validationErrors || [];
+      return new RestifyValidator(req, param, msg);
+    };
+
     return next();
   };
 };
