03a8ca3f96f84cb5e883ded2267adecc85e55b04
m-vdb <mvergerdelbove@work4labs.com>
2015-11-19 19:13:25 -0800

    payment/utils module

---
+++ ethic/payment/utils.js
@@ -0,0 +1,13 @@
+var config = require('config'),
+    stripe = require('stripe')(config.get('stripe.secretKey'));
+
+
+module.exports = {
+  stripe: stripe,
+  createCustomer: function (token, member, cb) {
+    stripe.customers.create({
+      source: token,
+      description: member._id.toString()
+    }, cb);
+  }
+}
